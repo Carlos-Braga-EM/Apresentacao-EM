9a4d152663333bf97f9b763220d574d3c348391a
Carlos-Braga-EM <carlos.gabriel@escolarmanager.com.br>
2025-09-02 15:05:15 -0300

    Add files via upload
    Generics

---
+++ Generics/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Produto(string nome, decimal valor, int quantidade)
+    {
+        public string Nome = nome;
+        public decimal Valor = valor;
+        public int Quantidade = quantidade;
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,25 @@
+﻿using Generics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Produto> listaDeProdutos = [
+                                            new("Sabonete", 12.9m, 10),
+                                            new("Condicionador", 18.9m, 12),
+                                            new("Shampoo", 21.9m, 0),
+                                            new("Lâmina de barbear", 4.99m, 16)
+                                        ];
+
+        //Produtos disponíveis
+        List<Produto> listaDeProdutosDisponiveis = listaDeProdutos.Where(p => p.Quantidade > 0).ToList();
+        
+        foreach(Produto p in listaDeProdutosDisponiveis)
+        {
+            Console.WriteLine(p.Nome); //Mostra os produtos disponíveis
+        }
+
+
+
+    }
+}
